528e059bffa87ab3a24550230eefdcddc05283e5
ninetrolls <s10241598@connect.np.edu.sg>
2024-05-02 13:57:18 +0800

    yes

--- books-api/app.js
+++ books-api/app.js
@@ -1,3 +1,4 @@
+//part 2
 const express = require('express');
 const bodyParser = require("body-parser");
 
@@ -20,3 +21,38 @@ app.use(bodyParser.urlencoded({ extended: true })); // Set extended: true for ne
 app.get('/books', (req, res) => {
     res.json(books); // Send the array of books as JSON response
  });
+
+ //part 3
+ app.post('/books', (req, res) => {
+    const newBook = req.body; // Get the new book data from the request body
+    newBook.id = books.length + 1; // Assign a unique ID
+    books.push(newBook); // Add the new book to the array
+    res.status(201).json(newBook); // Send created book with status code 201
+   });
+
+app.get('/books/:id', (req, res) => {
+const bookId = parseInt(req.params.id); // Get book ID from URL parameter
+const book = books.find(book => book.id === bookId);
+
+if (book) {
+    res.json(book); // Send the book data if found
+} else {
+    res.status(404).send('Book not found'); // Send error for non-existent book
+}
+});
+
+app.put('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id); // Get book ID from URL parameter
+    const updatedBook = req.body; // Get updated book data from request body
+  
+    const bookIndex = books.findIndex(book => book.id === bookId);
+  
+    if (bookIndex !== -1) {
+      updatedBook.id = bookId;
+      books[bookIndex] = updatedBook; // Update book data in the array
+      res.json(updatedBook); // Send updated book data
+    } else {
+      res.status(404).send('Book not found'); // Send error for non-existent book
+    }
+  });
+
